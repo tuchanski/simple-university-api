6eeaca59ff41772eb8c6a1b036755e6fe1e7781b
Tuchanski <gui.rocha2004@gmail.com>
2026-01-18 00:55:26 -0300

    update: api routes order

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfessorController;
 
 Route::prefix('professors')->name('professors.')->group(function () {
+    Route::post('/', [ProfessorController::class, 'store']);
     Route::get('/', [ProfessorController::class, 'index']);
     Route::get('/{id}', [ProfessorController::class, 'show']);
-    Route::post('/', [ProfessorController::class, 'store']);
     Route::delete('/{id}', [ProfessorController::class, 'destroy']);
     Route::patch('/{id}', [ProfessorController::class, 'update']);
 });
