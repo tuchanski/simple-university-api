11b42a401a9540c3cdcaa6f202413b4a7ff7bdb6
Tuchanski <gui.rocha2004@gmail.com>
2026-01-18 07:53:48 -0300

    feat: add base impl for student repo

--- app/Repositories/StudentRepository.php
+++ app/Repositories/StudentRepository.php
@@ -2,13 +2,47 @@
 
 namespace App\Repositories;
 
-class StudentRepository
+use App\Models\Student;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Collection;
+
+class StudentRepository implements BaseRepositoryInterface
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public function create(array $attributes) : Student
     {
-        //
+        return Student::create($attributes);
+    }
+
+    public function findById(int $id)
+    {
+        try
+        {
+            return Student::query()->findOrFail($id);
+        }
+        catch (ModelNotFoundException $e)
+        {
+            return null;
+        }
+    }
+
+    public function findAll(): Collection
+    {
+        return Student::all();
+    }
+
+    public function delete(int $id): bool
+    {
+        $student = Student::query()->findOrFail($id);
+        return $student->delete();
+    }
+
+    public function existsById(int $id): bool
+    {
+        return Student::query()->where('id', $id)->exists();
+    }
+
+    public function count(): int
+    {
+        return Student::query()->count();
     }
 }
