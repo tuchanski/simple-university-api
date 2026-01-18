874ae55c6f366f5f0f9699ae12ee8531473629f8
Tuchanski <gui.rocha2004@gmail.com>
2026-01-18 13:54:21 -0300

    feat: add course service interface

---
+++ app/Services/CourseService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Course;
+use Illuminate\Support\Collection;
+
+interface CourseService
+{
+    public function createCourse(array $data): Course;
+
+    public function getAllCourses(): Collection;
+
+    public function getCourseById(int $id): ?Course;
+
+    public function updateCourseById(int $id, array $data): Course;
+
+    public function deleteCourseById(int $id): void;
+}
