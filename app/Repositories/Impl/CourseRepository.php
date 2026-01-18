2293cdf331a9a436a0595d36a2d1ba35a109340b
Tuchanski <gui.rocha2004@gmail.com>
2026-01-18 13:52:22 -0300

    feat: add base course repo

---
+++ app/Repositories/Impl/CourseRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Repositories\Impl;
+
+use App\Models\Course;
+use App\Repositories\BaseRepositoryInterface;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Collection;
+
+class CourseRepository implements BaseRepositoryInterface
+{
+
+    public function create(array $attributes)
+    {
+        return Course::create($attributes);
+    }
+
+    public function findById(int $id)
+    {
+        try {
+            return Course::query()->findOrFail($id);
+        } catch (ModelNotFoundException $exception) {
+            return null;
+        }
+    }
+
+    public function findAll(): Collection
+    {
+        return Course::all();
+    }
+
+    public function delete(int $id): bool
+    {
+        $course = Course::query()->findOrFail($id);
+        return $course->delete();
+    }
+
+    public function existsById(int $id): bool
+    {
+        return Course::query()->where('id', $id)->exists();
+    }
+
+    public function count(): int
+    {
+        return Course::query()->count();
+    }
+
+    public function findAllByProfessorId(int $professorId): Collection {
+        return Course::query()->where('professor_id', $professorId)->get();
+    }
+
+}
