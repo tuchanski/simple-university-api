6651afe7489005c2d06bb1b3f929d6f44ff34b8a
Guilherme Tuchanski Rocha <guilherme.trocha@pucpr.br>
2026-01-20 15:01:40 -0300

    feat: implement basic jwt auth

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,22 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ProfessorController;
 use App\Http\Controllers\StudentController;
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api')->name('refresh');
+    Route::post('/me', [AuthController::class, 'me'])->middleware('auth:api')->name('me');
+});
 
 Route::prefix('professors')->name('professors.')->group(function () {
     Route::post('/', [ProfessorController::class, 'store']);
