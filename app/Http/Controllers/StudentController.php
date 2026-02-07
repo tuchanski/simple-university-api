ce06f6e1e0d90f8402312a1d93649e79c71de81a
Guilherme Tuchanski Rocha <gui.rocha2004@gmail.com>
2026-02-07 16:57:50 -0300

    feat: add more docs to student controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -12,6 +12,7 @@ use App\Helpers\Utilities;
 use App\Services\Impl\StudentServiceImpl;
 use App\Services\StudentService;
 use Dedoc\Scramble\Attributes\PathParameter;
+use Dedoc\Scramble\Attributes\Response;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
@@ -32,6 +33,7 @@ class StudentController extends Controller
      *
      * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
      */
+    #[Response(401, 'Unauthenticated', type: 'array{message: "Unauthenticated"}')]
     public function index()
     {
         return response($this->studentService->getAllStudents(), 200);
@@ -45,6 +47,9 @@ class StudentController extends Controller
      * @param Request $request
      * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
      */
+    #[Response(400, 'Bad Request', type: 'array{message: "Invalid property"}')]
+    #[Response(401, 'Unauthorized', type: 'array{message: "Unauthorized"}')]
+    #[Response(201, 'Student Created')]
     public function store(Request $request)
     {
 
@@ -62,7 +67,7 @@ class StudentController extends Controller
                 'address' => 'required',
             ]);
 
-            return response(['data' => $this->studentService->createStudent($request->all())], 201);
+            return response($this->studentService->createStudent($request->all()), 201);
         } catch (EmailAlreadyRegisteredException|InvalidEmailException|InvalidGenderException|ValidationException $exception) {
             return GlobalExceptionHandler::retrieveResponse($exception);
         }
@@ -77,10 +82,13 @@ class StudentController extends Controller
      * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
      */
     #[PathParameter('id', description: 'The ID of the student being shown', type: 'integer', example: '1')]
+    #[Response(404, 'Not Found', type: 'array{message: "Student not found"}')]
+    #[Response(200, 'Student Found', type: 'array{id: "0", name: "string", email: "string@email.com",
+    gender: "male", phone: "41999999999", address: "Rua X, 123"}')]
     public function show(int $id)
     {
         try {
-            return response(['data' => $this->studentService->getStudentById($id)], 200);
+            return response($this->studentService->getStudentById($id), 200);
         } catch (StudentNotFoundException $exception) {
             return GlobalExceptionHandler::retrieveResponse($exception);
         }
@@ -96,6 +104,10 @@ class StudentController extends Controller
      * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
      */
     #[PathParameter('id', description: 'The ID of the student being updated', type: 'integer', example: '1')]
+    #[Response(404, 'Not Found', type: 'array{message: "Student not found"}')]
+    #[Response(400, 'Bad Request', type: 'array{message: "Invalid property"}')]
+    #[Response(401, 'Unauthorized', type: 'array{message: "Unauthorized"}')]
+    #[Response(200, 'Course Updated', type: 'array{message: "Student updated successfully"}')]
     public function update(int $id, Request $request)
     {
 
@@ -113,7 +125,9 @@ class StudentController extends Controller
                 'address' => '',
             ]);
 
-            return response(['data' => $this->studentService->updateStudentById($id, $request->all())], 200);
+            $this->studentService->updateStudentById($id, $request->all());
+
+            return response(['message' => 'Student updated successfully'], 200);
         } catch (EmailAlreadyRegisteredException|StudentNotFoundException|InvalidEmailException|InvalidGenderException|ValidationException $exception) {
             return GlobalExceptionHandler::retrieveResponse($exception);
         }
@@ -128,6 +142,8 @@ class StudentController extends Controller
      * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
      */
     #[PathParameter('id', description: 'The ID of the student being deleted', type: 'integer', example: '1')]
+    #[Response(404, 'Not Found', type: 'array{message: "Student not found"}')]
+    #[Response(401, 'Unauthorized', type: 'array{message: "Unauthorized"}')]
     public function destroy(int $id)
     {
 
